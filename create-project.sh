8ddd2279e1db5d95c84b5d49e9c7fe57e723ae98
local <joaobianchi@live.com>
2021-06-16 00:25:43 -0300

    :construction: fix git configs && magento cmds

--- create-project.sh
+++ create-project.sh
@@ -5,5 +5,5 @@
 cd /var/www/html/$STORENAME
 find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
 find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
-chown -R :www-data . # Ubuntu
+chown -R www-data:www-data /var/www/html/$STORENAME # Ubuntu
 chmod u+x bin/magento
