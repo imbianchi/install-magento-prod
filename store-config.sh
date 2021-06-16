ccefa08d49ad6c277625fbb6bf310c8edb86bb28
local <joaobianchi@live.com>
2021-06-15 23:58:45 -0300

    :construction: fix config magento commands

--- store-config.sh
+++ store-config.sh
@@ -28,56 +28,56 @@ php bin/magento setup:install \
           --page-cache-redis-server=localhost \
           --page-cache-redis-db=2
 echo "Running bistwobis configs..."
-php bin/magento config:set \
-          --admin/security/admin_account_sharing 1 \
-          --admin/security/lockout_failures '0' \
-          --admin/security/lockout_threshold '' \
-          --admin/security/password_is_forced 0 \
-          --admin/security/password_lifetime '' \
-          --admin/security/session_lifetime 31536000 \
-          --admin/security/use_form_key 0 \
-          --cataloginventory/options/show_out_of_stock 1
-          --customer/password/lockout_threshold 0 \
-          --customer/password/required_character_classes_number 2 \
-          --dev/image/default_adapter IMAGEMAGICK \
-          --general/country/allow BR \
-          --general/country/default BR \
-          --general/country/destinations BR \
-          --general/country/optional_zip_countries BR \
-          --general/locale/code pt_BR \
-          --general/locale/timezone America/Sao_Paulo \
-          --general/locale/weight_unit kgs \
-          --general/region/display_all 1 \
-          --general/region/state_required BR \
-          --general/single_store_mode/enabled 1 \
-          --general/store_information/country_id BR \
-          --general/store_information/region_id 499 \
-          --general/store_information/name 'Bis2bis E-commerce' \
-          --general/store_information/phone '43 3326-1500' \
-          --general/store_information/postcode '86050-435' \
-          --general/store_information/city 'Londrina' \
-          --general/store_information/street_line1 'Bento Munhoz da Rocha Neto' \
-          --general/store_information/merchant_vat_number '10.738.352/0001-00' \
-          --oauth/access_token_lifetime/customer '' \
-          --oauth/access_token_lifetime/admin '' \
-          --oauth/consumer/expiration_period 31536000 \
-          --sales_email/general/async_sending 1 \
-          --sitemap/generate/enabled 1 \
-          --sitemap/generate/time '02,00,00' \
-          --sitemap/generate/frequency D \
-          --shipping/origin/country_id BR \
-          --shipping/origin/region_id 499 \
-          --shipping/origin/postcode 86050-435 \
-          --shipping/origin/city Londrina \
-          --shipping/origin/street_line1 'Rua Bento Munhoz da Rocha Neto' \
-          --system/backup/functionality_enabled 1 \
-          --system/currency/installed BRL \
-          --system/upload_configuration/jpeg_quality 90 \
-          --system/upload_configuration/max_height 1080 \
-          --web/seo/use_rewrites 1 \
-          --web/secure/use_in_frontend 1 \
-          --web/secure/use_in_adminhtml 1
-          --cleanup-database
+php bin/magento config:set admin/dashboard/enable_charts 1 &&
+php bin/magento config:set --admin/security/admin_account_sharing 1 &&
+php bin/magento config:set --admin/security/lockout_failures '0' &&
+php bin/magento config:set --admin/security/lockout_threshold '' &&
+php bin/magento config:set --admin/security/password_is_forced 0 &&
+php bin/magento config:set --admin/security/password_lifetime '' &&
+php bin/magento config:set --admin/security/session_lifetime 31536000 &&
+php bin/magento config:set --admin/security/use_form_key 0 &&
+php bin/magento config:set --cataloginventory/options/show_out_of_stock 1
+php bin/magento config:set --customer/password/lockout_threshold 0 &&
+php bin/magento config:set --customer/password/required_character_classes_number 2 &&
+php bin/magento config:set --dev/image/default_adapter IMAGEMAGICK &&
+php bin/magento config:set --general/country/allow BR &&
+php bin/magento config:set --general/country/default BR &&
+php bin/magento config:set --general/country/destinations BR &&
+php bin/magento config:set --general/country/optional_zip_countries BR &&
+php bin/magento config:set --general/locale/code pt_BR &&
+php bin/magento config:set --general/locale/timezone America/Sao_Paulo &&
+php bin/magento config:set --general/locale/weight_unit kgs &&
+php bin/magento config:set --general/region/display_all 1 &&
+php bin/magento config:set --general/region/state_required BR &&
+php bin/magento config:set --general/single_store_mode/enabled 1 &&
+php bin/magento config:set --general/store_information/country_id BR &&
+php bin/magento config:set --general/store_information/region_id 499 &&
+php bin/magento config:set --general/store_information/name 'Bis2bis E-commerce' &&
+php bin/magento config:set --general/store_information/phone '43 3326-1500' &&
+php bin/magento config:set --general/store_information/postcode '86050-435' &&
+php bin/magento config:set --general/store_information/city 'Londrina' &&
+php bin/magento config:set --general/store_information/street_line1 'Bento Munhoz da Rocha Neto' &&
+php bin/magento config:set --general/store_information/merchant_vat_number '10.738.352/0001-00' &&
+php bin/magento config:set --oauth/access_token_lifetime/customer '' &&
+php bin/magento config:set --oauth/access_token_lifetime/admin '' &&
+php bin/magento config:set --oauth/consumer/expiration_period 31536000 &&
+php bin/magento config:set --sales_email/general/async_sending 1 &&
+php bin/magento config:set --sitemap/generate/enabled 1 &&
+php bin/magento config:set --sitemap/generate/time '02,00,00' &&
+php bin/magento config:set --sitemap/generate/frequency D &&
+php bin/magento config:set --shipping/origin/country_id BR &&
+php bin/magento config:set --shipping/origin/region_id 499 &&
+php bin/magento config:set --shipping/origin/postcode 86050-435 &&
+php bin/magento config:set --shipping/origin/city Londrina &&
+php bin/magento config:set --shipping/origin/street_line1 'Rua Bento Munhoz da Rocha Neto' &&
+php bin/magento config:set --system/backup/functionality_enabled 1 &&
+php bin/magento config:set --system/currency/installed BRL &&
+php bin/magento config:set --system/upload_configuration/jpeg_quality 90 &&
+php bin/magento config:set --system/upload_configuration/max_height 1080 &&
+php bin/magento config:set --web/seo/use_rewrites 1 &&
+php bin/magento config:set --web/secure/use_in_frontend 1 &&
+php bin/magento config:set --web/secure/use_in_adminhtml 1
+--cleanup-database
 
 php bin/magento config:set dev/static/sign 0
 php bin/magento setup:static-content:deploy -f
