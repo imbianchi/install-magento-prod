13db807dbbedbce787a3476b314b68ae8e7c5092
local <joaobianchi@live.com>
2021-06-16 00:00:41 -0300

    :construction: fix git config path

--- git-config.sh
+++ git-config.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Configura o GIT depois da loja instalada
+cd /var/www/html/$STORENAME
 sudo -H -u www-data bash -c "git init"
 sudo -H -u www-data bash -c "git remote add origin $GITURL"
 sudo -H -u www-data bash -c "git remote set-url origin $GITURL"
