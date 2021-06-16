7f4146ccddeec583ed83560cd1de1fc892c56e19
local <joaobianchi@live.com>
2021-06-16 01:14:28 -0300

    :construction: fix git configs && magento cmds

--- git-config.sh
+++ git-config.sh
@@ -4,9 +4,9 @@
 cd /var/www/html/$STORENAME
 
 sudo -H -u www-data bash -c "git init"
-sudo -H -u www-data bash -c "git remote add origin $GIREPO"
-sudo -H -u www-data bash -c "git remote set-url origin $GIREPO"
+sudo -H -u www-data bash -c "git remote add origin $GITREPO"
+sudo -H -u www-data bash -c "git remote set-url origin $GITREPO"
 sudo -H -u www-data bash -c "git fetch --all "
 sudo -H -u www-data bash -c "git reset --hard origin/$GITBRANCH"
-sudo -H -u www-data bash -c "git pull origin $GITBRANCH -f"
 sudo -H -u www-data bash -c "git checkout $GITBRANCH -f"
+sudo -H -u www-data bash -c "git pull origin $GITBRANCH -f"
