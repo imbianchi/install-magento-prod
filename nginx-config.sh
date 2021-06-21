cc5c16c8ebfe28f6684af1d644171ca6b1c0a111
local <joaobianchi@live.com>
2021-06-21 08:01:16 -0300

    :art: :construction: :fire: fixing, improving code and removing files

--- nginx-config.sh
+++ nginx-config.sh
@@ -4,11 +4,20 @@ request_uri='$request_uri'
 host='$host'
 http_x_forwarded_proto='$http_x_forwarded_proto'
 MAGE_ROOT='$MAGE_ROOT'
-
-# create the respective directory
-cd /etc/nginx/conf.d/
-touch ./$STORENAME.conf &&
-echo "server {
+port="listen 80;"
+serverDevName="server_name $STORENAME.bisws.com.br;"
+serverProdName="server_name $STORENAME.com.br  www.$STORENAME.com.br;"
+setServerDir="set $MAGE_ROOT /var/www/html/$STORENAME;"
+accessLog="access_log /var/log/nginx/$STORENAME-access.log;"
+errorLog="error_log /var/log/nginx/$STORENAME-error.log;"
+includeSample="include /etc/nginx/conf.d/nginx.conf.sample;"
+redirectsTo="
+        #redirect to HTTPS behind ELB
+        if ($http_x_forwarded_proto != 'https') {
+            return 301 https://$host$request_uri;
+          }
+"
+serverConfig="
         client_max_body_size 100M;
         proxy_connect_timeout 600; 
         proxy_send_timeout 600; 
@@ -17,19 +26,48 @@ echo "server {
         client_header_timeout 600;
         client_body_timeout 600;
         fastcgi_read_timeout 600;
+"
 
-        listen 80;
+developConfig="server {
+        $serverConfig
+
+        $port
         
-        server_name $STORENAME.bisws.com.br;
-        set $MAGE_ROOT /var/www/html/$STORENAME;
+        $serverDevName
+        $setServerDir
         
-      #redirect to HTTPS behind ELB
-       if ($http_x_forwarded_proto != 'https') {
-         return 301 https://$host$request_uri;
-      }
+        $redirectsTo
 
-        access_log /var/log/nginx/$STORENAME-access.log;
-        error_log /var/log/nginx/$STORENAME-error.log;
+        $accessLog
+        $errorLog
 
-       include /etc/nginx/conf.d/nginx.conf.sample;
-}" >> ./$STORENAME.conf
+       $includeSample
+}"
+
+productionConfig="server {
+        $serverConfig
+
+        $port
+        
+        $serverProdName
+        $setServerDir
+        
+        $redirectsTo
+
+        $accessLog
+        $eerorLog
+
+       $includeSample
+}"
+
+# create the respective file in the respective directory
+if [[ $GITBRANCH = 'main' ]] || [[ $GITBRANCH = 'master' ]]
+    then
+        cd /etc/nginx/sites-enabled/
+        touch ./$STORENAME.conf &&
+        echo productionConfig >> ./$STORENAME.conf
+    else
+        cd /etc/nginx/conf.d/
+        touch ./$STORENAME &&
+        echo developConfig >> ./$STORENAME
+fi
