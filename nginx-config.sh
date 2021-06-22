291a662763a31a30e7e8babb59c6d721f57366d1
local <joaobianchi@live.com>
2021-06-21 21:13:06 -0300

    :art: :construction: fixing nginx config dev/prod mode

--- nginx-config.sh
+++ nginx-config.sh
@@ -4,20 +4,12 @@ request_uri='$request_uri'
 host='$host'
 http_x_forwarded_proto='$http_x_forwarded_proto'
 MAGE_ROOT='$MAGE_ROOT'
-port="listen 80;"
-serverDevName="server_name $STORENAME.bisws.com.br;"
-serverProdName="server_name $STORENAME.com.br  www.$STORENAME.com.br;"
-setServerDir="set $MAGE_ROOT /var/www/html/$STORENAME;"
-accessLog="access_log /var/log/nginx/$STORENAME-access.log;"
-errorLog="error_log /var/log/nginx/$STORENAME-error.log;"
-includeSample="include /etc/nginx/conf.d/nginx.conf.sample;"
-redirectsTo="
-        #redirect to HTTPS behind ELB
-        if ($http_x_forwarded_proto != 'https') {
-            return 301 https://$host$request_uri;
-          }
-"
-serverConfig="
+
+if [[ $GITBRANCH = 'main' ]] || [[ $GITBRANCH = 'master' ]]
+    then
+        cd /etc/nginx/sites-enabled/
+        touch ./$STORENAME &&
+        echo "server {
         client_max_body_size 100M;
         proxy_connect_timeout 600; 
         proxy_send_timeout 600; 
@@ -26,48 +18,49 @@ serverConfig="
         client_header_timeout 600;
         client_body_timeout 600;
         fastcgi_read_timeout 600;
-"
 
-developConfig="server {
-        $serverConfig
-
-        $port
+        listen 80;
         
-        $serverDevName
-        $setServerDir
+        server_name $STORENAME.com.br  www.$STORENAME.com.br;
+        set $MAGE_ROOT /var/www/html/$STORENAME;
         
-        $redirectsTo
+        #redirect to HTTPS behind ELB
+          if ($http_x_forwarded_proto != 'https') {
+             return 301 https://$host$request_uri;
+         }
 
-        $accessLog
-        $errorLog
+        access_log /var/log/nginx/$STORENAME-access.log;
+        error_log /var/log/nginx/$STORENAME-error.log;
 
-       $includeSample
-}"
-
-productionConfig="server {
-        $serverConfig
-
-        $port
-        
-        $serverProdName
-        $setServerDir
-        
-        $redirectsTo
-
-        $accessLog
-        $eerorLog
-
-       $includeSample
-}"
-
-# create the respective file in the respective directory
-if [[ $GITBRANCH = 'main' ]] || [[ $GITBRANCH = 'master' ]]
-    then
-        cd /etc/nginx/sites-enabled/
-        touch ./$STORENAME.conf &&
-        echo $productionConfig >> ./$STORENAME.conf
+       include /etc/nginx/conf.d/nginx.conf.sample;
+}" >> ./$STORENAME
     else
         cd /etc/nginx/conf.d/
-        touch ./$STORENAME &&
-        echo $developConfig >> ./$STORENAME
+        touch ./$STORENAME.conf &&
+        echo "server {
+        client_max_body_size 100M;
+        proxy_connect_timeout 600; 
+        proxy_send_timeout 600; 
+        proxy_read_timeout 600; 
+        send_timeout 600;
+        client_header_timeout 600;
+        client_body_timeout 600;
+        fastcgi_read_timeout 600;
+
+        listen 80;
+        
+        server_name $STORENAME.bisws.com.br;
+        set $MAGE_ROOT /var/www/html/$STORENAME;
+        
+        #redirect to HTTPS behind ELB
+          if ($http_x_forwarded_proto != 'https') {
+            return 301 https://$host$request_uri;
+        }
+
+        access_log /var/log/nginx/$STORENAME-access.log;
+        error_log /var/log/nginx/$STORENAME-error.log;
+
+       include /etc/nginx/conf.d/nginx.conf.sample;
+}" >> ./$STORENAME.conf
+        
 fi
